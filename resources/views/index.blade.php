46cf487f3ddcd4b962b75b735af0bd3f562f9657
Awais <awaistayyab27@gmail.com>
2025-01-06 12:47:25 +0500

    Auto commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -179,8 +179,10 @@
 
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <div class="section-title">
+                        @if(!empty($blog))
                         <h3 class="color-pink"><a href="{{route('category.show' , Crypt::encrypt($blog->category_id))}}"
                                 title="">{{ $blog->category->name }}</a></h3>
+                        @endif
                     </div>
                     <div class="row">
                         @foreach ($blogs->skip(1)->take(2) as $blog)
